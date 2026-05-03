a72f35d39018d726060810595b337abb48f33d40
Neal Morton <mortonne@gmail.com>
2026-05-03 11:30:19 -0500

    fix: package name

--- src/hamltools/figures.py
+++ src/hamltools/figures.py
@@ -7,5 +7,5 @@ import matplotlib.pyplot as plt
 def set_style(style_path=None):
     """Set default plot style."""
     if style_path is None:
-        style_path = resources.files('hamllab').joinpath('figures.mplstyle')
+        style_path = resources.files('hamltools').joinpath('figures.mplstyle')
     plt.style.use(style_path)
